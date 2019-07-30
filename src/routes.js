76802f628a4820a3a4321387c0619b4ca77a7fef
aharken <31554609+aharken@users.noreply.github.com>
2019-07-30 12:09:53 -0700

    Naming conventions for Zone 1,2, Lighting 1, 2, and added page for Zone 2

--- src/routes.js
+++ src/routes.js
@@ -92,12 +92,12 @@ const routes = [
   { path: '/charts', name: 'Charts', component: Charts },
   { path: '/users', exact: true,  name: 'Users', component: Users },
   { path: '/users/:id', exact: true, name: 'User Details', component: User },
-  { path: '/thermostat/overview', name: 'Thermostat', component: Thermostat },
-  { path: '/thermostat/zone1', name: 'ThermZone1', component: ThermZone1 },
-  { path: '/thermostat/zone2', name: 'ThermZone2', component: ThermZone2 },
+  { path: '/thermostat/overview', name: 'Thermostat Overview', component: Thermostat },
+  { path: '/thermostat/zone1', name: 'Thermostat Zone 1', component: ThermZone1 },
+  { path: '/thermostat/zone2', name: 'Thermostat Zone 2', component: ThermZone2 },
   { path: '/lighting/overview', name: 'Lighting', component: Lighting },
-  { path: '/lighting/room1', name: 'LightRoom1', component: LightRoom1 },
-  { path: '/lighting/room2', name: 'LightRoom2', component: LightRoom2 },
+  { path: '/lighting/room1', name: 'Lighting Room 1', component: LightRoom1 },
+  { path: '/lighting/room2', name: 'Lighting Room 2', component: LightRoom2 },
 ];
 
 export default routes;
